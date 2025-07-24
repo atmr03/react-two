6963a5362b4a682b3ae624e918f1285a8113daac
unknown <martstepen03@gmail.com>
2025-07-24 09:10:17 +0700

    kedua

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,43 @@ function App() {
   const handleClick = () => {
     setLike(like + 1);
   };
+  const [emot, setEmot] = useState("😜");
+  const handleEmot = (e) => {
+    setEmot(e.target.textContent);
+  };
   return (
-    <button
-      onClick={handleClick}
-      className="p-11 bg-slate-500 flex justify-center items-center h-auto mx-auto text-white font-bold"
-    >
-      👍{like}
-    </button>
+    <>
+      <button
+        onClick={handleClick}
+        className="p-11 bg-slate-500 flex justify-center items-center h-auto mx-auto text-white font-bold"
+      >
+        👍{like}
+      </button>
+
+      <div className="flex flex-col justify-center items-center h-screen text-9xl ">
+        <div className="p-10">{emot}</div>
+        <div className="flex text-3xl font-bold gap-8">
+          <button
+            onClick={handleEmot}
+            className="p-5 bg-white rounded-lg shadow-xl"
+          >
+            🤧
+          </button>
+          <button
+            onClick={handleEmot}
+            className="p-5 bg-white rounded-lg shadow-xl"
+          >
+            🤣
+          </button>
+          <button
+            onClick={handleEmot}
+            className="p-5 bg-white rounded-lg shadow-xl"
+          >
+            😜
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
